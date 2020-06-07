8b2e4d5fc418fa0f45447399c1c99eefcbc86ab3
M-Sufyan22 <mmuhammadsufyan5@gmail.com>
2020-06-07 18:24:10 +0500

    modified

--- app.js
+++ app.js
@@ -47,3 +47,57 @@ function myFunction2() {
         x.type = "password";
     }
 }
+
+
+//  Jquery Work
+
+$('.buttons').click(function() {
+    var buttonvalue = $(this).attr('data-filter');
+
+    if (buttonvalue == 'all') {
+        $('.image-filter').show('1000');
+    } else {
+        $('.image-filter').not('.' + buttonvalue).hide('200');
+        $('.image-filter').filter('.' + buttonvalue).show('200');
+    }
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
+// For LOgin Form
+
+$(document).ready(function() {
+    $("#search").click(function() {
+        $(".search").show();
+        $(".fa-close").show();
+    });
+    $(".fa-close").click(function() {
+        $(".search").hide();
+        $(".fa-close").hide();
+    });
+
+    $(".signup-form").hide();
+    $(".log").addClass("activee");
+    $(".fa-eye-slash").hide();
+
+    $(".sign").click(function() {
+        $(".signup-form").show();
+        $(".login-form").hide();
+        $(".sign").addClass("activee");
+        $(".log").removeClass("activee");
+    });
+
+    $(".log").click(function() {
+        $(".sign").removeClass("activee");
+        $(".log").addClass("activee");
+        $(".signup-form").hide();
+        $(".login-form").show();
+    });
+    $(".fa-eye").click(function() {
+        $(".fa-eye").hide();
+        $(".fa-eye-slash").show();
+    });
+    $(".fa-eye-slash").click(function() {
+        $(".fa-eye-slash").hide();
+        $(".fa-eye").show();
+    });
+});
