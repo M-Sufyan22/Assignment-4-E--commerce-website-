1cdf51c9160e42def17dc523ade03e63d8998971
M-Sufyan22 <mmuhammadsufyan5@gmail.com>
2020-07-07 01:35:39 +0500

    pre loader and go to top feature added

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
 // JAVA SCRIPT Work
 
+// pre loader
+
+function bodyload() {
+    document.getElementById("pre-loader").style.display = "none";
+}
+
+// go to top
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".nav-sec").style.top = "0";
+        } else {
+            document.querySelector(".nav-sec").style.top = "-100px";
+        }
+        if (prevScrollpos < currentScrollPos) {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+        prevScrollpos = currentScrollPos;
+
+    }
+    // go to top function
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// 
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+})
+
+
 // time counter
 
 var countDownDate = new Date("jun 30, 2021 24:00:00").getTime();
@@ -47,6 +102,10 @@ function myFunction2() {
         x.type = "password";
     }
 }
+
+// touch-slider = carousel 
+
+
 
 
 //  Jquery Work
@@ -101,3 +160,6 @@ $(document).ready(function() {
         $(".fa-eye").show();
     });
 });
+
+
+//
